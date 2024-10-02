c90a76d9b66dfa36674fe54a7f78ca7b812afe67
CalebLink18 <143741278+CalebLink18@users.noreply.github.com>
2024-10-01 21:40:09 -0400

    Add files via upload

---
+++ Untitled4.ps1
@@ -0,0 +1,58 @@
+﻿function gatherClasses(){
+
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.10/Courses-1.html
+
+$trs=$page.ParsedHtml.body.getElementsByTagName("tr")
+
+$FullTable = @()
+for ($i=1; $i -lt $trs.length; $i++){
+
+    $tds = $trs[$i].getElementsByTagName("td") # <-- probably incorrect
+
+    $times = $tds[5].innerText.split("-") # cannot index into a null array
+
+    $FullTable += [PSCustomObject]@{ # somethings wrong
+    
+        "Class Code" = $tds[0].innerText; `
+        "Title" = $tds[1].innerText; `
+        "Days" = $tds[4].innerText; `
+        "Time Start" = $times[0]; `
+        "Time End" = $times[1]; `
+        "Instructor" = $tds[5].innerText; `
+        "Location" = $tds[8].innerText ; `
+
+    }
+
+}
+
+return $FullTable
+
+}
+
+function daysTranslator($FullTable){
+
+for ($i=0; $i -lt $FullTable.length; $i++) {
+
+    $days = @()
+
+    if($FullTable[$i].Days -ilike "*M*"){ $days += "Monday" }
+
+    if($FullTable[$i].Days -ilike "*T*" -or $FullTable[$i].Days -ilike "*TU*"){ $days += "Tuesday" }
+
+    if($FullTable[$i].Days -ilike "*W*"){ $days += "Wendsday" }
+
+    if($FullTable[$i].Days -ilike "*TH*"){ $days += "Thursday" }
+
+    if($FullTable[$i].Days -ilike "*F*"){ $days += "Friday" }
+
+    $FullTable[$i].Days = $days
+
+}
+
+return $FullTable
+
+}
+
+$FullTable = gatherClasses
+$FullTable = daysTranslator($FullTable)
+$FullTable
