1d97c77fd64e4960e4b9f66ff8258668a980a777
CalebLink18 <143741278+CalebLink18@users.noreply.github.com>
2024-10-15 16:38:16 -0400

    Add files via upload

---
+++ Turn to Menu/Functions2.ps1
@@ -0,0 +1,88 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+
+function choiceCheck($string) {
+
+if ($string -ne {1-5}) {return $false}
+
+}
+
+function last10Logs() {
+
+$logsNotFormatted = get-content C:\xampp\apache\logs\access.log
+$tableRecords = @{}
+
+for ($i=0; $i -lt $logsNotFormatted.Count ;$i++ ){
+
+$words = $logsNotFormatted[$i] -split " ";
+
+                $tableRecords += [PSCustomObject]@{
+                           "IP" = $words[0];
+                           "Time" = $words[3].Trim('[');
+                           "Method" = $words[5].Trim('"');
+                           "Page" = $words[6];
+                           "Protocol" = $words[7];
+                           "Response" = $words[8];
+                           "Referrer" = $words[9];
+                           "Client" = $words[11..($words. ...)];}
+
+}
+return $tableRecords | Where-Object { $_.IP -like "10.*"}
+
+}
+
+function failedLogins10() {
+  
+  $failedlogins = Get-EventLog security | Where { $_.InstanceID -eq "4625" }
+
+  $failedloginsTable = @()
+  for($i=0; $i -lt $failedlogins.Count; $i++){
+
+    $account=""
+    $domain="" 
+
+    $usrlines = getMatchingLines $failedlogins[$i].Message "*Account Name*"
+    $usr = $usrlines[1].Split(":")[1].trim()
+
+    $dmnlines = getMatchingLines $failedlogins[$i].Message "*Account Domain*"
+    $dmn = $dmnlines[1].Split(":")[1].trim()
+
+    $user = $dmn+"\"+$usr;
+
+    $failedloginsTable += [pscustomobject]@{"Time" = $failedlogins[$i].TimeGenerated; `
+                                       "Id" = $failedlogins[$i].InstanceId; `
+                                    "Event" = "Failed"; `
+                                     "User" = $user;
+                                     }
+    }
+    $chickenTendies = $failedloginsTable | Select-Object -First 10
+
+    return $chickenTendies
+}
+
+
+function failedLoginsAll() {
+  
+  $failedlogins = Get-EventLog security | Where { $_.InstanceID -eq "4625" }
+
+  $failedloginsTable = @()
+  for($i=0; $i -lt $failedlogins.Count; $i++){
+
+    $account=""
+    $domain="" 
+
+    $usrlines = getMatchingLines $failedlogins[$i].Message "*Account Name*"
+    $usr = $usrlines[1].Split(":")[1].trim()
+
+    $dmnlines = getMatchingLines $failedlogins[$i].Message "*Account Domain*"
+    $dmn = $dmnlines[1].Split(":")[1].trim()
+
+    $user = $dmn+"\"+$usr;
+
+    $failedloginsTable += [pscustomobject]@{"Time" = $failedlogins[$i].TimeGenerated; `
+                                       "Id" = $failedlogins[$i].InstanceId; `
+                                    "Event" = "Failed"; `
+                                     "User" = $user;
+                                     }
+    }
+    return $failedloginsTable
+}
---
+++ Turn to Menu/main-2.ps1
@@ -0,0 +1,60 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+. (Join-Path $PSScriptRoot Functions2.ps1)
+
+clear
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - Display last 10 logs`n"
+$Prompt += "2 - Display last 10 failed logs`n"
+$Prompt += "3 - Display at risk users`n"
+$Prompt += "4 - Champlain.com`n"
+$Prompt += "5 - Exit"
+
+
+$operation = $true
+
+while($operation){
+
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+    $TF = choiceCheck($choice)
+    if($tf -ne $true) { Write-Host "incorrect input try again"; $choice = 0 }
+
+    #exit
+    if($choice -eq 5){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    #Display last 10 apache logs
+    elseif($choice -eq 1){
+        $lastLogs = last10Logs
+        Write-Host ($lastLogs | Format-Table | Out-String)
+    }
+
+    #Display last 10 failed logins for all users
+    elseif($choice -eq 2){
+        $failedLogins = failedLogins10
+        Write-Host ($failedLogins | Format-Table | Out-String)
+    }
+
+    #Display at risk users
+    elseif($choice -eq 3){ 
+        $failedLoginsAll = failedLoginsAll
+        Write-Host ($failedLoginsAll | Format-Table | Out-String)
+    }
+
+    #Start chrome and navigate to champlain.edu
+    elseif($choice -eq 4){
+
+    $MinecraftIsBetter = Get-Process -Name "chrome.exe"
+    if($MinecraftIsBetter) {Start-Process chrome.exe "champlain.edu"}
+           
+    }
+    
+    else { Write-Host "thats not a choice ya dumb cunt, try again" }
+}
